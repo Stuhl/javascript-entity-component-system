ddf18f19360b55a9a5ea97978527e77e148c45c6
Stuhl <BasicsDone7@web.de>
2019-04-19 22:46:12 +0200

    weird

--- src/example.js
+++ src/example.js
@@ -1,84 +1,84 @@
-// Components (just data and state)
-const gravityComponent = {
-  name: "gravity",
-  state: {
-    gravity: true
-  }
-}
-
-const positionComponent = {
-  name: "position",
-  state: {
-    x: 0,
-    y: 0
-  }
-}
-
-const dragComponent = {
-  name: "drag",
-  state: {
-    drag: true
-  }
-}
-
-const accelerationComponent = {
-  name: "acceleration",
-  state: {
-    acceleration: 5
-  }
-}
-
-// Processors (handling logic)
-const gravityProcessor = {
-  name: "gravityProcessor",
-  component: "gravity",
-  update(component, entities) {
-    entities.forEach(entity => {
-      entity.y += 9.81
-    })
-  }
-}
-
-const dragProcessor = {
-  name: "dragProcessor",
-  component: "drag",
-  update(component, entities) {
-    entities.forEach(entity => {
-      entity.x -= 2
-    })
-  }
-}
-
-const accelerationProcessor = {
-  name: "accelerationProcessor",
-  component: "acceleration",
-  update(component, entities) {
-    entities.forEach(entity => {
-      entity.x += component.state.acceleration
-    })
-  }
-}
-
-const ecs = new EntityComponentSystem()
-
-ecs.addProcessor(gravityProcessor)
-ecs.addProcessor(dragProcessor)
-ecs.addProcessor(accelerationProcessor)
-
-
-ecs.addComponent(accelerationComponent)
-ecs.addComponent(gravityComponent)
-ecs.addComponent(positionComponent)
-ecs.addComponent(dragComponent)
-// ecs.displayProcessors()
-
-const player = ecs.createEntity("player", ["position", "gravity"])
-const enemy = ecs.createEntity("enemy", ["position", "gravity"])
-const car = ecs.createEntity("mustang", ["position", "gravity", "drag", "acceleration"])
-
-ecs.addEntity(player)
-ecs.addEntity(enemy)
-ecs.addEntity(car)
-ecs.update()
-
-console.log(ecs)
+// // Components (just data and state)
+// const gravityComponent = {
+//   name: "gravity",
+//   state: {
+//     gravity: true
+//   }
+// }
+//
+// const positionComponent = {
+//   name: "position",
+//   state: {
+//     x: 0,
+//     y: 0
+//   }
+// }
+//
+// const dragComponent = {
+//   name: "drag",
+//   state: {
+//     drag: true
+//   }
+// }
+//
+// const accelerationComponent = {
+//   name: "acceleration",
+//   state: {
+//     acceleration: 5
+//   }
+// }
+//
+// // Processors (handling logic)
+// const gravityProcessor = {
+//   name: "gravityProcessor",
+//   component: "gravity",
+//   update(component, entities) {
+//     entities.forEach(entity => {
+//       entity.y += 9.81
+//     })
+//   }
+// }
+//
+// const dragProcessor = {
+//   name: "dragProcessor",
+//   component: "drag",
+//   update(component, entities) {
+//     entities.forEach(entity => {
+//       entity.x -= 2
+//     })
+//   }
+// }
+//
+// const accelerationProcessor = {
+//   name: "accelerationProcessor",
+//   component: "acceleration",
+//   update(component, entities) {
+//     entities.forEach(entity => {
+//       entity.x += component.state.acceleration
+//     })
+//   }
+// }
+//
+// const ecs = new EntityComponentSystem()
+//
+// ecs.addProcessor(gravityProcessor)
+// ecs.addProcessor(dragProcessor)
+// ecs.addProcessor(accelerationProcessor)
+//
+//
+// ecs.addComponent(accelerationComponent)
+// ecs.addComponent(gravityComponent)
+// ecs.addComponent(positionComponent)
+// ecs.addComponent(dragComponent)
+// // ecs.displayProcessors()
+//
+// const player = ecs.createEntity("player", ["position", "gravity"])
+// const enemy = ecs.createEntity("enemy", ["position", "gravity"])
+// const car = ecs.createEntity("mustang", ["position", "gravity", "drag", "acceleration"])
+//
+// ecs.addEntity(player)
+// ecs.addEntity(enemy)
+// ecs.addEntity(car)
+// ecs.update()
+//
+// console.log(ecs)
